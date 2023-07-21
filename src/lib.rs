b3bae9f2ee769b7333490e38c7abeaffd0bda949
Antoine Vandermeersch <avdmeers@gmail.com>
2023-07-22 00:09:48 +0200

    refactoring for custom window

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,18 @@ mod utils;
 
 extern crate fixedbitset;
 extern crate rand;
+extern crate web_sys;
 
 use fixedbitset::FixedBitSet;
 use rand::prelude::*;
+use std::collections::VecDeque;
 use wasm_bindgen::prelude::*;
+
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
 
 #[wasm_bindgen]
 pub struct Universe {
@@ -55,19 +63,48 @@ impl Universe {
     }
 
     pub fn tick(&mut self, pattern: &Pattern) {
+        utils::set_panic_hook();
+        log!("computing tick");
+
         let width = (2 * self.span + 1) as usize;
-        let mut carry_over = self.cells[width-1];
-        let start = self.cells[0];
+        let rs = ((pattern.r - 1) / 2) as usize;
 
-        for j in 0..width {
-            let k = if j == width - 1 { start } else { self.cells[j + 1] };
-            let bits = [carry_over, self.cells[j], k];
+        let mut carry_over: VecDeque<bool> = VecDeque::with_capacity(rs);
+        (width-rs..width).for_each(|i| {
+            carry_over.push_back(self.cells[i]);
+        });
 
-            let outcome = pattern.outcome(&bits).unwrap();
+        let mut start = Vec::with_capacity(rs);
+        (0..rs).for_each(|i| {
+            start.push(self.cells[i]);
+        });
 
-            carry_over = self.cells[j];
-            self.cells.set(j, outcome);
+        for i in 0..width {
+            let mut bits = Vec::from(carry_over.as_slices().0);
+            bits.push(self.cells[i]);
+
+            if i < width-rs {
+                for offset in 1..rs+1 {
+                    bits.push(self.cells[i + offset]);
+                }
+            } else {
+                for j in i+1..width {
+                    bits.push(self.cells[j]);
+                }
+
+                bits.extend(&start[0..(rs - width + i + 1)]);
+            }
+
+            let outcome = pattern.outcome(&bits[..]).unwrap();
+
+            carry_over.pop_front();
+            carry_over.push_back(self.cells[i]);
+            carry_over.make_contiguous();
+
+            self.cells.set(i, outcome);
         }
+
+        log!("last cell: {}", self.cells[width-1]);
     }
 
     pub fn randomize(&mut self) {
